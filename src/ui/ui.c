87445a520f91168b96bfe2d5620f87734fbef6f4
ranlong479-byte <ranlong479@gmail.com>
2026-04-25 21:40:51 +0800

    feat:实现模块的循环播放

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -1,67 +1,106 @@
 #include "ui.h"
 
-
-static void btn_event_cb(lv_event_t* e)
+typedef enum
 {
-    lv_obj_t* obj=lv_event_get_target(e);
+    RED = 0xff0000,
+    GREEN = 0x00ff00,
+    BLUE = 0x0000ff
+} color_t;
+static color_t color[] = {BLUE, RED, GREEN, BLUE, RED};
+static uint8_t center_index = 2;
+bool is_code_scroll = false;
+/*obj对象*/
+lv_obj_t *obj;
+void scrollbar_cb(lv_event_t *e)
+{
+    if (is_code_scroll)
+    {
+        is_code_scroll = false;
+        return;
+    }
+    lv_obj_t *target = lv_event_get_target(e);
+    int32_t scroll_x = lv_obj_get_scroll_x(target);
 
-    lv_obj_set_style_bg_color(obj, lv_color_hex(0x000000), LV_PART_MAIN);
+    /*往左滚动*/
+    if (scroll_x > 300)
+    {
+        if (center_index == 4)
+        {
+            center_index = 1;
+        }
+        center_index++;
+    }
+    /*向右滚动*/
+    if (scroll_x < 300)
+    {
+        if (center_index == 0)
+        {
+            center_index = 3;
+        }
+        center_index--;
+    }
+    /*重铺背景颜色*/
+    for (size_t i = 0; i < 3; i++)
+    {
+        lv_obj_t *child = lv_obj_get_child(target, i);
+        lv_obj_set_style_bg_color(child, lv_color_hex(color[center_index + i - 1]), LV_PART_MAIN);
+    }
+
+    /*移动元素到原来的显示效果*/
+    if (scroll_x > 300)
+    {
+        is_code_scroll = true;
+        lv_obj_scroll_to_x(obj, scroll_x - 300, LV_ANIM_OFF);
+    }
+    if (scroll_x < 300)
+    {
+        is_code_scroll = true;
+        lv_obj_scroll_to_x(obj, scroll_x + 300, LV_ANIM_OFF);
+    }
+}
+
+void clear_default_style(lv_obj_t *o)
+{
+    /*去圆角*/
+    lv_obj_set_style_radius(o, 0, LV_PART_MAIN);
+    /*边框*/
+    lv_obj_set_style_border_width(o, 0, LV_PART_MAIN);
+    /*外边框*/
+    lv_obj_set_style_pad_all(o, 0, LV_PART_MAIN);
+    lv_obj_set_style_pad_column(o, 0, LV_PART_MAIN);
+    lv_obj_set_style_pad_row(o, 0, LV_PART_MAIN);
 }
 void ui_init(void)
 {
     //! 获取屏幕活动对像
     lv_obj_t *screen = lv_screen_active();
     /*创建元素对象*/
-    lv_obj_t *obj = lv_obj_create(screen);
-
+    obj = lv_obj_create(screen);
+    clear_default_style(obj);
     /*设置元素样式*/
-    lv_obj_set_size(obj, 320, 480);
+    lv_obj_set_size(obj, 300, 300);
     lv_obj_set_pos(obj, 100, 100);
-    lv_obj_set_style_radius(obj, 0, LV_PART_MAIN);
-    lv_obj_set_style_border_width(obj, 0, LV_PART_MAIN);
-    lv_obj_set_style_pad_all(obj, 0, LV_PART_MAIN);
-    // lv_obj_set_scrollbar_mode(obj, LV_SCROLLBAR_MODE_OFF);
-    lv_obj_set_style_bg_color(obj, lv_color_hex(0xffffff), LV_PART_MAIN);
 
-    /*设置为网格布局*/
+    static int32_t col[] = {300, 300, 300, LV_GRID_TEMPLATE_LAST};
+    static int32_t row[] = {300, LV_GRID_TEMPLATE_LAST};
+
     lv_obj_set_layout(obj, LV_LAYOUT_GRID);
-
-    /*设置为3行3列*/
-    static int32_t column_dsc[] = {100, 100, 100, LV_GRID_TEMPLATE_LAST}; /* 2 columns with 100- and 400-px width */
-    static int32_t row_dsc[] = {100, 100, 100, LV_GRID_TEMPLATE_LAST};
-    lv_obj_set_grid_dsc_array(obj, column_dsc, row_dsc);
-
-    lv_obj_t *label;
-    lv_obj_t *btn;
-    uint8_t i;
-    for (i = 0; i < 9; i++)
+    lv_obj_set_grid_dsc_array(obj, col, row);
+    /*关闭滚动条*/
+    lv_obj_set_scrollbar_mode(obj, LV_SCROLLBAR_MODE_OFF);
+    /*关闭惯性滚动*/
+    lv_obj_remove_flag(obj, LV_OBJ_FLAG_SCROLL_MOMENTUM);
+    for (size_t i = 0; i < 3; i++)
     {
-        if (i == 7)
-            continue;
-        uint8_t col = i % 3;
-        uint8_t row = i / 3;
+        lv_obj_t *child = lv_obj_create(obj);
+        clear_default_style(child);
+        /*将child添加到cell中*/
+        lv_obj_set_grid_cell(child, LV_GRID_ALIGN_STRETCH, i, 1, LV_GRID_ALIGN_STRETCH, 0, 1);
 
-        btn = lv_obj_create(obj);
-        /*Stretch the cell horizontally and vertically too
-         *Set span to 1 to make the cell 1 column/row sized*/
-        lv_obj_set_style_bg_color(btn, lv_color_hex(0x80D6FF), LV_PART_MAIN);
-        lv_obj_set_grid_cell(btn, LV_GRID_ALIGN_STRETCH, col, 1,
-                             LV_GRID_ALIGN_STRETCH, row, 1);
-        if (i == 6)
-        {
-            lv_obj_set_grid_cell(btn, LV_GRID_ALIGN_STRETCH, col, 2,
-                                 LV_GRID_ALIGN_STRETCH, row, 1);
-        }
-        if (i == 8)
-        {
-            lv_obj_set_grid_cell(btn, LV_GRID_ALIGN_STRETCH, col, 1,
-                                 LV_GRID_ALIGN_STRETCH, row - 1, 2);
-            lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_CLICKED, NULL);
-        }
-
-        label = lv_label_create(btn);
-        lv_label_set_text_fmt(label, "c%d,r%d", col, row);
-
-        lv_obj_center(label);
+        lv_obj_set_style_bg_color(child, lv_color_hex(color[center_index + i - 1]), LV_PART_MAIN);
     }
+
+    lv_obj_add_event_cb(obj, scrollbar_cb, LV_EVENT_SCROLL_END, NULL);
+    /*移动到中心*/
+    lv_obj_scroll_to_x(obj, 300, LV_ANIM_OFF);
 }
